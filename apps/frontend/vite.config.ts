e8572cc28837f99f7ee3d3a07c74078d2d79343c
Mamang <90405274+Mamang007@users.noreply.github.com>
2026-05-18 22:12:11 +0700

    Initialize project library and tech stack
    - Installed TanStack Router, Query, Axios, Zod, and Prisma.
    - Configured backend with Helmet and Cors.
    - Created shared Zod schema.
    - Set up basic frontend routing and query provider.
    - Initialized Prisma in backend.
    
    Closes #1

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import path from 'path'
 
 // https://vite.dev/config/
@@ -8,7 +9,10 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, path.resolve(__dirname, '../../'), '')
   
   return {
-    plugins: [react()],
+    plugins: [
+      TanStackRouterVite(),
+      react()
+    ],
     envDir: '../../',
     server: {
       proxy: {
